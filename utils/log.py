986b6a43a7ed0ef2977715ae26c847350fa7fc10
Fucloud <270928474@qq.com>
2023-08-09 00:28:07 +0800

    修改 日志系统为静态函数

--- utils/log.py
+++ utils/log.py
@@ -20,40 +20,54 @@ class LogType(Enum):
 
 class Log:
     __output_path = Path("log")
+    __log_file = None
+    __show_on_cli = True
+    __show_on_file = False
 
-    def __init__(self, show_on_cli: bool = True):
-        # 创建文件名
-        self.__log_save_path: Path
-        self.__log_file = None
-        self.__create()
-
-        self.show_on_cli = show_on_cli
+    # 静态函数初始化
+    @staticmethod
+    def init(show_on_cli: bool = True, show_on_file: bool = True):
+        Log.__create()
+        Log.show_on_cli = show_on_cli
+        Log.__show_on_file = show_on_file
 
     # 创建log文件
-    def __create(self):
-        if not Log.__output_path.exists():
-            Log.__output_path.mkdir()
+    @staticmethod
+    def __create():
+        try:
+            if not Log.__output_path.exists():
+                Log.__output_path.mkdir()
 
-        # 创建文件
-        log_file_name = Path(time.get_log_file_time() + ".log")
-        log_file_name = Path.joinpath(Log.__output_path, log_file_name)
-        self.__log_file = open(log_file_name, "w", encoding="utf-8")
+            # 创建文件
+            log_file_name = Path(time.get_log_file_time() + ".log")
+            log_file_name = Path.joinpath(Log.__output_path, log_file_name)
+            Log.__log_file = open(log_file_name, "w", encoding="utf-8")
 
-    def __check_valid(self) -> bool:
-        return self.__log_file is not None
+            raise FileNotFoundError
+        except Exception:
+            Log.__log(LogType.error, "Log文件创建失败")
 
-    def __log(self, msg_type: LogType, args: tuple):
-        if not self.__check_valid():
-            return
+    @staticmethod
+    def __check_valid() -> bool:
+        return Log.__log_file is not None
 
+    @staticmethod
+    def __log(msg_type: LogType, *args):
         # 生成文本
-        output_text = "[%s]%-7s" % (time.get_log_time(), f"[{msg_type}]")
+        raw_log_msg = ""
         for arg in args:
-            output_text += " " + str(arg)
+            raw_log_msg += str(arg) + ' '
 
-        if self.show_on_cli:
-            print(output_text)
-        self.__log_file.write(output_text + '\n')
+        log_msg = Log.__gen_log_msg(msg_type, raw_log_msg)
+        if Log.__show_on_cli:
+            print(log_msg, end="")
+        if Log.__show_on_file and Log.__check_valid():
+            Log.__log_file.write(log_msg)
+
+    # 生成日志信息
+    @staticmethod
+    def __gen_log_msg(msg_type: LogType, msg: str) -> str:
+        return "[%s]%-7s %s\n" % (time.get_log_time(), f"[{msg_type}]", msg)
 
     # 清除所有日志文件
     @staticmethod
@@ -64,19 +78,22 @@ class Log:
         for file in Log.__output_path.iterdir():
             file.unlink()
 
-    def info(self, *args):
-        self.__log(LogType.info, args)
+    @staticmethod
+    def info( *args):
+        Log.__log(LogType.info, *args)
 
-    def warn(self, *args):
-        self.__log(LogType.warn, args)
+    @staticmethod
+    def warn(*args):
+        Log.__log(LogType.warn, *args)
 
-    def error(self, *args):
-        self.__log(LogType.error, args)
+    @staticmethod
+    def error(*args):
+        Log.__log(LogType.error, *args)
 
 
 if __name__ == "__main__":
     Log.clear()
-    log = Log()
-    log.info("hello", 1, 2, 3)
-    log.error("file not found")
-    log.warn("you are wrong")
+    Log.init()
+    Log.info("hello", 1, 2, 3)
+    Log.error("file not found")
+    Log.warn("you are wrong")
