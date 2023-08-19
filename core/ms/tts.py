295ad463b6a569ef2e511d50b0fad8256ca97763
Fucloud <270928474@qq.com>
2023-08-19 16:29:47 +0800

    refactor: 优化文本输入方式 1. 自动生成默认输入文件 2. 将输出路径嵌入输入文件中 3. 使程序能够忽略空行和注释文字 4. 自动备份输入文本

--- core/ms/tts.py
+++ core/ms/tts.py
@@ -6,27 +6,31 @@
 
 from datetime import datetime
 from pathlib import Path
+import shutil
 
 from core.ms.tts_sdk import Speech
 from utils.log import Log
 from utils import time
 from utils.system import exit_program
 
+# 默认输入文件
+default_input_file_name = "input.txt"
+# 注释文本
+example_note = "// 以下第一行为输出文件，接下来的所有行会被转换为语音\n" \
+    "// 注意程序会忽略“//”开头的文本和空行"
+
 
 class TTS:
     __speak_texts = []
 
-    def __init__(self, name: str = "tts",
-                 output_file_path: str = "output",
-                 name_len: int = 5):
+    def __init__(self, output_path: str = 'output', name_len: int = 5):
         # 初始化 加载配置信息+连接api
         self.speech = Speech()
         self.speech.connect()
 
-        self.name = name
-        self.output_path = Path(output_file_path)
-        if not self.output_path.exists():
-            self.output_path.mkdir()
+        self.name = None
+        self.input_path: Path = Path()
+        self.output_path: Path = Path(output_path)
 
         # name_len 用于个输入message命名(输入文本的前n个)
         self.name_len = max(3, name_len)
@@ -36,18 +40,37 @@ class TTS:
         self.__speak_texts = speak_texts
 
     # 文件输入
-    def read_speak_text(self, read_file_path: str):
+    def read_speak_text(self, read_file_path: str = default_input_file_name):
         try:
             with open(read_file_path, "r", encoding="utf-8") as f:
                 read_texts = f.readlines()
         except FileNotFoundError:
-            Log.error("{} not exists!".format(read_file_path))
+            if read_file_path == default_input_file_name:
+                gen_default_input_file()
+                Log.error("默认输入文件不存在，已创建", default_input_file_name)
+            else:
+                Log.error("{} 文件不存在".format(read_file_path))
+
             exit_program(-1)
 
-        for text in read_texts:
-            # 去除空值
-            if text != "":
+        #  取第一个元素为项目名字
+        counter = 0
+        for i in range(0, len(read_texts)):
+            text = read_texts[i].strip()
+            # 如果文本非空 而且并非//开头 则加入
+            if text.startswith("//") or text is None:
+                continue
+            elif counter == 0:
+                self.name = read_texts[i].strip()
+            else:
                 self.__speak_texts.append(text)
+            counter += 1
+
+        if counter < 2:
+            Log.error("没有输入“输出路径”或“转换文本”")
+            exit_program(-1)
+
+        self.input_path = Path(read_file_path)
 
     # 文本转语音
     def speak(self):
@@ -58,6 +81,9 @@ class TTS:
         for i in range(len(self.__speak_texts)):
             speak_text = self.__speak_texts[i]
             self.__to_sound(output_path, speak_text, i)
+
+        # 备份输入信息
+        shutil.copy(self.input_path, Path.joinpath(output_path, self.input_path.name))
 
         # 显示结果
         Log.info("合成音频完成! 共{}个文本, 耗时: {}s".format(
@@ -87,7 +113,7 @@ class TTS:
         if index == -1:
             return file_name
         else:
-            return f"[{index + 1}] {file_name}"
+            return f"[%02d] %s" % (index + 1, file_name)
 
     # 设置输出路径
     def __gen_output_path(self, begin_time: datetime) -> Path:
@@ -104,3 +130,7 @@ class TTS:
 
         return output_path
 
+
+def gen_default_input_file():
+    with open(default_input_file_name, "w", encoding="utf-8") as f:
+        f.write(example_note)
