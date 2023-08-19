a3563011d7f33b9b3f74d6d4dcf47e7c922f7bd1
Fucloud <270928474@qq.com>
2023-08-19 16:30:53 +0800

    feat: 封装自动打包程序

---
+++ tools/pyinstaller.py
@@ -0,0 +1,9 @@
+# -*- coding = utf-8 -*-
+# @Time : 2023/08/19 15:58
+# @Autor : Fucloud
+# @FIle : pyinstaller.py.py
+# @Software : PyCharm
+import os
+
+if __name__ == "__main__":
+    os.system("pyinstaller main.spec")
